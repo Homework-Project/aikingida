7be155d12e645adae164dfef267441465410e5d5
Abdulrasheed <soladnet@hotmail.com>
2013-12-06 16:25:10 +0100

    installing backbone using shim

--- www/js/index.js
+++ www/js/index.js
@@ -30,3 +30,35 @@
 //    }
 //};
 
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+    },
+    shim: {
+        'backbone': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['underscore', 'jquery'],
+            //Once loaded, use the global 'Backbone' as the
+            //module value.
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
+
+// Start the main app logic.
+requirejs(['jquery', 'backbone'],
+        function($, Backbone) {
+            //jQuery, canvas and the app/sub module are all
+            //loaded and can be used here now.
+            testFunction(4);
+        });
