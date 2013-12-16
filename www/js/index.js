c81e04cf3d99fc22a6c051d6d1df507d36e88cef
Abdulrasheed <soladnet@hotmail.com>
2013-12-16 16:02:15 +0100

    index fixes from ola

--- www/js/index.js
+++ www/js/index.js
@@ -1,34 +1,3 @@
-//var app = {
-//    // Application Constructor
-//    initialize: function() {
-//        this.bindEvents();
-//    },
-//    // Bind Event Listeners
-//    //
-//    // Bind any events that are required on startup. Common events are:
-//    // 'load', 'deviceready', 'offline', and 'online'.
-//    bindEvents: function() {
-//        document.addEventListener('deviceready', this.onDeviceReady, false);
-//    },
-//    // deviceready Event Handler
-//    //
-//    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-//    // function, we must explicity call 'app.receivedEvent(...);'
-//    onDeviceReady: function() {
-//        app.receivedEvent('deviceready');
-//    },
-//    // Update DOM on a Received Event
-//    receivedEvent: function(id) {
-//        var parentElement = document.getElementById(id);
-//        var listeningElement = parentElement.querySelector('.listening');
-//        var receivedElement = parentElement.querySelector('.received');
-//
-//        listeningElement.setAttribute('style', 'display:none;');
-//        receivedElement.setAttribute('style', 'display:block;');
-//
-//        console.log('Received Event: ' + id);
-//    }
-//};
 
 requirejs.config({
     //By default load any module IDs from js/lib
@@ -60,23 +29,23 @@ requirejs.config({
 requirejs(['jquery', 'backbone'],
         function($, Backbone) {
             $(document).ready(function() {
-                $.ajax("http://soladnet.com/phonegap-api.php"/*"http://localhost/Login/index.php"*/, {
-                    dataType: "json",
-                    type: "POST",
-                    data:{
-                        a:"a"
-                    },
-                    success: function(response, statusText, xhr) {
-//                        $("#content").html(response);
-                        alert(0);
-                    },
-                    error: function(resp, status, error) {
-                        alert(status)
-                        
-                    },
-                    complete: function(resp, status) {
-                        alert(status);
-                    }
-                });
+//                $.ajax("http://soladnet.com/phonegap-api.php"/*"http://localhost/Login/index.php"*/, {
+//                    dataType: "json",
+//                    type: "POST",
+//                    data:{
+//                        a:"a"
+//                    },
+//                    success: function(response, statusText, xhr) {
+////                        $("#content").html(response);
+//                        alert(0);
+//                    },
+//                    error: function(resp, status, error) {
+//                        alert(status)
+//                        
+//                    },
+//                    complete: function(resp, status) {
+//                        alert(status);
+//                    }
+//                });
             });
         });
